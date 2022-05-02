3ef6b0f76812cf7b7e6e77e1bde9414b4116c6b1
James <james@wallofhistory.com>
2022-05-01 22:23:22 -0500

    Update lstmGAN.py

--- lstmGAN/lstmGAN.py
+++ lstmGAN/lstmGAN.py
@@ -12,25 +12,19 @@ To import a saved model
 
 model = load_model('./LSTM_generator.h5')"""
 
-from tensorflow.keras.datasets import mnist
 # from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Bidirectional, LSTM, Reshape, RepeatVector,
 # TimeDistributed
 
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Model
 # from keras.optimizers import Adam
-from numpy.random import rand, randint, randn
-import tensorflow as tf
+from numpy.random import rand
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, LeakyReLU, Input, Dense, Bidirectional, LSTM, \
+from tensorflow.keras.layers import Dropout, LeakyReLU, Input, Dense, Bidirectional, LSTM, \
     RepeatVector, TimeDistributed
-from tensorflow.keras.layers import Reshape, Conv2DTranspose
 from dataloader import DataLoader
-import matplotlib.pyplot as plt
 import numpy as np
 import os
-import re
-
 
 run_gpu = input('Do you want to run on the GPU? : y/n    ')
 if run_gpu == 'n':
@@ -61,7 +55,7 @@ class LSTMGAN:
         self.generator = self.build_generator()
 
         # The generator takes noise as input and generates song
-        z = Input(shape=(self.dl.getMaxDimension(),self.dl.getMaxDimension()))
+        z = Input(shape=(self.dl.getMaxDimension(), self.dl.getMaxDimension()))
 
         img = self.generator(z)
 
@@ -79,23 +73,24 @@ class LSTMGAN:
     def build_generator(self):
 
         model = Sequential()
-        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True), input_shape=(self.dl.getMaxDimension(), self.dl.getMaxDimension())))
+        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True),
+                                input_shape=(self.dl.getMaxDimension(), self.dl.getMaxDimension())))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True)))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount())))
         model.add(LeakyReLU(alpha=0.2))
 
-        #specifying output to have 40 timesteps
+        # specifying output to have 40 timesteps
         model.add(RepeatVector(pow(self.dl.getMaxDimension(), 2)))
-        #specifying 1 feature as the output
-        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True, dropout = 0.2)))
+        # specifying 1 feature as the output
+        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True, dropout=0.2)))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True, dropout = 0.2)))
+        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True, dropout=0.2)))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True, dropout = 0.2)))
+        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), return_sequences=True, dropout=0.2)))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.3))   
+        model.add(Dropout(0.3))
         model.add(TimeDistributed(Dense(self.dl.getUniqueWordCount())))
 
         model.add(LeakyReLU(alpha=0.2))
@@ -107,8 +102,7 @@ class LSTMGAN:
         model.add(LeakyReLU(alpha=0.2))
         model.summary()
 
-
-        noise = Input(shape=(self.dl.getMaxDimension(),self.dl.getMaxDimension()))
+        noise = Input(shape=(self.dl.getMaxDimension(), self.dl.getMaxDimension()))
 
         img = model(noise)
 
@@ -118,33 +112,32 @@ class LSTMGAN:
 
         model = Sequential()
 
-
-        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), activation = 'relu', return_sequences=True), input_shape=(pow(self.dl.getMaxDimension(), 2), 1)))
+        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), activation='relu', return_sequences=True),
+                                input_shape=(pow(self.dl.getMaxDimension(), 2), 1)))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), activation = 'relu')))
+        model.add(Bidirectional(LSTM(self.dl.getUniqueWordCount(), activation='relu')))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.4))
         model.add(RepeatVector(1))
-        model.add(TimeDistributed(Dense(self.dl.getUniqueWordCount(), activation = 'sigmoid')))
+        model.add(TimeDistributed(Dense(self.dl.getUniqueWordCount(), activation='sigmoid')))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.4))
-        model.add(TimeDistributed(Dense(self.dl.getUniqueWordCount(), activation = 'relu')))
+        model.add(TimeDistributed(Dense(self.dl.getUniqueWordCount(), activation='relu')))
 
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.4))
         model.add(TimeDistributed(Dense(1, activation='linear')))
         model.summary()
 
-
-        img = Input(shape=(pow(self.dl.getMaxDimension(), 2),1))
+        img = Input(shape=(pow(self.dl.getMaxDimension(), 2), 1))
 
         validity = model(img)
 
         return Model(img, validity)
-    
+
     def predict_and_save(self, epoch):
-		
-		# get the word dictionary from the data loader
+
+        # get the word dictionary from the data loader
         wordDict = self.dl.getSongWordDict(scaled=False)
 
         file = open('./savedSongs/generator_model_' + str(epoch) + '_songs.txt', 'w')
@@ -153,19 +146,19 @@ class LSTMGAN:
             prediction = self.generator.predict(random)
             prediction = prediction * self.dl.getUniqueWordCount()
 
-            prediction = prediction.reshape(self.dl.getMaxDimension()*self.dl.getMaxDimension(), 1)
+            prediction = prediction.reshape(self.dl.getMaxDimension() * self.dl.getMaxDimension(), 1)
             prediction = prediction.astype(int)
 
-            #np.save('gan_prediction', prediction)
-    
+            # np.save('gan_prediction', prediction)
+
             song = []
             previousWord = None
             for word in prediction:
                 # it is likely that the exact number predicted will not
                 # match up to a value in the wordDict
                 # so we will find the closest one that does
-                wordDictValues = list(wordDict.values()) 
-                        
+                wordDictValues = list(wordDict.values())
+
                 if not word == 0:
                     wordPredicted = list(wordDict.keys())[list(wordDict.values()).index(word)]
                     wordPredicted = wordPredicted.strip()
@@ -177,7 +170,7 @@ class LSTMGAN:
                 file.write(' '.join(song))
                 file.write('\n--------------------\n')
         file.close()
-    
+
     def train(self, epochs, batch_size, save_interval):
 
         # Load the dataset
@@ -201,13 +194,13 @@ class LSTMGAN:
             imgs = X_train[idx]
             imgs = np.array(imgs)
 
-            imgs = imgs.reshape(len(imgs),pow(self.dl.getMaxDimension(), 2),1)
+            imgs = imgs.reshape(len(imgs), pow(self.dl.getMaxDimension(), 2), 1)
 
             # Sample noise and generate a batch of new songs
-            noise = np.random.normal(0, 1, (batch_size,self.dl.getMaxDimension(),self.dl.getMaxDimension()))
+            noise = np.random.normal(0, 1, (batch_size, self.dl.getMaxDimension(), self.dl.getMaxDimension()))
 
             gen_imgs = self.generator.predict(noise)
-                       
+
             # Train the discriminator (real classified as ones and generated as zeros)
             d_loss_real = self.discriminator.train_on_batch(imgs, valid)
             d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
@@ -225,7 +218,7 @@ class LSTMGAN:
 
             # If at save interval => save model
             if epoch % save_interval == 0:
-	        # generate a few songs with this current generator
+                # generate a few songs with this current generator
                 self.predict_and_save(epoch)
                 self.generator.save(f"./saved-model-states/LSTM_generator_epoch_{epoch}.h5")
 
@@ -333,12 +326,11 @@ class LSTMGAN:
 #             return i
 #         else:
 #             return self.getDimensions(a, i + 1)
-        
+
 if __name__ == '__main__':
     dl = DataLoader()
     dl.loadData()
-    #print(dl.getMaxDimension())
-    #print(dl.getUniqueWordCount())
+    # print(dl.getMaxDimension())
+    # print(dl.getUniqueWordCount())
     lstmgan = LSTMGAN()
     lstmgan.train(epochs=20, batch_size=20, save_interval=2)
-
